fa360e30aafb003c3553fbe1413ab2c50444105c
Alexander Pankratov <a.pankratov@astarus.com>
2023-09-05 11:21:11 +0400

    change in isolate bus

--- lib/src/event_bus_isolate.dart
+++ lib/src/event_bus_isolate.dart
@@ -45,10 +45,14 @@ class EventBusIsolate implements EventBusStream {
 
   void _init() async {
     _receivePort = ReceivePort();
-    _isolate = await Isolate.spawn<SendPort>(_worker, _receivePort!.sendPort);
-    _toEBSender = await _receivePort!.first;
+
+    _isolate = await Isolate.spawn(_worker, [_receivePort!.sendPort, onInit]);
+    // _toEBSender = await _receivePort!.first;
     //from isolate eb to stream eb
     _receivePort!.listen((message) {
+      if (message is SendPort) {
+        _toEBSender = message;
+      }
       if (message is (EventDTO<dynamic>, dynamic)) {
         //call result TODO:
         print('get call result');
@@ -61,30 +65,33 @@ class EventBusIsolate implements EventBusStream {
       _toEBSender!.send(event);
     });
   }
+}
 
-  void _worker(SendPort sendPort) async {
-    ReceivePort innerReceivePort = ReceivePort();
-    sendPort.send(innerReceivePort.sendPort);
-    var eventBus = onInit();
+void _worker(dynamic data) async {
+  EventBus Function() onInit = data[1];
 
-    var listenerSend = eventBus.streamSend.listen((event) {
-      sendPort.send(event);
-    });
-    var listenerCall = eventBus.streamCall.listen((event) {
-      sendPort.send(event);
-    });
+  ReceivePort innerReceivePort = ReceivePort();
+  SendPort sendPort = data[0];
+  sendPort.send(innerReceivePort.sendPort);
+  var eventBus = onInit();
 
-    await for (var message in innerReceivePort) {
-      // if (message is _CallEventDTO) {
-      if (message is EventDTO) {
-        eventBus.sinkToCall.add(message);
-      } else if (message is EventDTO) {
-        //if (message is _SendEventDTO) {
-        eventBus.sinkToSend.add(message);
-      }
+  var listenerSend = eventBus.streamSend.listen((event) {
+    sendPort.send(event);
+  });
+  var listenerCall = eventBus.streamCall.listen((event) {
+    sendPort.send(event);
+  });
+
+  await for (var message in innerReceivePort) {
+    // if (message is _CallEventDTO) {
+    if (message is EventDTO) {
+      eventBus.sinkToCall.add(message);
+    } else if (message is EventDTO) {
+      //if (message is _SendEventDTO) {
+      eventBus.sinkToSend.add(message);
     }
-    listenerSend.cancel();
-    listenerCall.cancel();
-    innerReceivePort.close();
   }
+  listenerSend.cancel();
+  listenerCall.cancel();
+  innerReceivePort.close();
 }

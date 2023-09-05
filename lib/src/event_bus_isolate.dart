827b387e6d67bc17d30ddd158b292fd478086bf8
Alexander Pankratov <a.pankratov@astarus.com>
2023-09-05 09:55:44 +0400

    add stream eb to isolate eb

--- lib/src/event_bus_isolate.dart
+++ lib/src/event_bus_isolate.dart
@@ -8,7 +8,25 @@ class _CallEventDTO extends EventDTOImpl {
   _CallEventDTO(super.topic, super.data);
 }
 
-class EventBusIsolate {
+class EventBusIsolate implements EventBusStream {
+  final StreamController<EventDTO> _toEBStreamController = StreamController.broadcast();
+  final StreamController<EventDTO> _fromEBStreamController = StreamController.broadcast();
+  @override
+  // TODO: implement sinkToCall
+  Sink<EventDTO> get sinkToCall => throw UnimplementedError();
+
+  @override
+  // TODO: implement sinkToSend
+  Sink<EventDTO> get sinkToSend => _toEBStreamController.sink;
+
+  @override
+  // TODO: implement streamCall
+  Stream<(EventDTO, dynamic)> get streamCall => throw UnimplementedError();
+
+  @override
+  // TODO: implement streamSend
+  Stream<EventDTO> get streamSend => _fromEBStreamController.stream;
+
   ///this func call from other isolate
   EventBus Function() onInit;
   Isolate? _isolate;
@@ -29,6 +47,19 @@ class EventBusIsolate {
     _receivePort = ReceivePort();
     _isolate = await Isolate.spawn(_worker, _receivePort!.sendPort);
     _toEBSender = await _receivePort!.first;
+    //from isolate eb to stream eb
+    _receivePort!.listen((message) {
+      if (message is (EventDTO<dynamic>, dynamic)) {
+        //call result TODO:
+        print('get call result');
+      } else if (message is EventDTO) {
+        _fromEBStreamController.add(message);
+      }
+    });
+    //from stream eb to isolate eb
+    _toEBStreamController.stream.listen((event) {
+      _toEBSender!.send(event);
+    });
   }
 
   void _worker(SendPort sendPort) async {
@@ -44,9 +75,11 @@ class EventBusIsolate {
     });
 
     await for (var message in innerReceivePort) {
-      if (message is _CallEventDTO) {
+      // if (message is _CallEventDTO) {
+      if (message is EventDTO) {
         eventBus.sinkToCall.add(message);
-      } else if (message is _SendEventDTO) {
+      } else if (message is EventDTO) {
+        //if (message is _SendEventDTO) {
         eventBus.sinkToSend.add(message);
       }
     }
